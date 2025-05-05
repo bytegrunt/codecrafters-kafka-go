3b5284376d7953ff17deeae18aa86003c9bee129
bytegrunt <207869117+bytegrunt@users.noreply.github.com>
2025-05-05 19:21:29 -0400

    send correlation id

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -26,4 +27,26 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	writeResponse(conn)
+}
+
+func writeResponse(conn net.Conn) {
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("message_size: %d\r\ncorrelation_id: %d\r\n", 5, 7))
+
+	conn.Write([]byte(builder.String()))
 }
